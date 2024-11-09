4fa6eed16ecf0ece84211a6883ae4c431abf1e72
Mahardicka <mahardicka404@gmail.com>
2024-11-09 23:43:02 +0700

    date formater

---
+++ src/utils/date.ts
@@ -0,0 +1,25 @@
+export function formatDateToIndonesian(dateString: string): string {
+  const [datePart] = dateString.split(' ');
+  const date = new Date(datePart);
+
+  const months = [
+    'Januari',
+    'Februari',
+    'Maret',
+    'April',
+    'Mei',
+    'Juni',
+    'Juli',
+    'Agustus',
+    'September',
+    'Oktober',
+    'November',
+    'Desember',
+  ];
+
+  const day = date.getDate();
+  const month = months[date.getMonth()];
+  const year = date.getFullYear();
+
+  return `${day} ${month} ${year}`;
+}
