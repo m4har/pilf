9b2b8144e7d3d484016ff836534ad64bd97e4e00
Mahardicka <mahardicka404@gmail.com>
2024-11-09 23:43:28 +0700

    constant filter

---
+++ src/utils/constant.ts
@@ -0,0 +1,22 @@
+export const FILTER = [
+  {
+    label: 'URUTKAN',
+    key: '',
+  },
+  {
+    label: 'Nama A-Z',
+    key: 'asc',
+  },
+  {
+    label: 'Nama Z-A',
+    key: 'desc',
+  },
+  {
+    label: 'Tanggal Terbaru',
+    key: 'newDate',
+  },
+  {
+    label: 'Tanggal Terlama',
+    key: 'oldDate',
+  },
+];
