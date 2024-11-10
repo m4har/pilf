139be49df9af30366a7dfaf58f637c8971b2ced1
Mahardicka <mahardicka404@gmail.com>
2024-11-10 14:18:25 +0700

    component label for detail description

---
+++ src/components/label/label-desc.tsx
@@ -0,0 +1,19 @@
+import {Text, View, ViewStyle} from 'react-native';
+
+type Props = {
+  title: string;
+  desc: string;
+  style?: ViewStyle;
+};
+
+export const LabelDesc = (props: Props) => {
+  const {title, desc, style} = props;
+  return (
+    <View style={style}>
+      <Text style={{fontWeight: 'bold', textTransform: 'uppercase'}}>
+        {title}
+      </Text>
+      <Text>{desc}</Text>
+    </View>
+  );
+};
